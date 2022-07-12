be62af2a84e766e5e2bf14cd93c431fe51858e94
Shubham <singhshubham1211@gmail.com>
2022-07-12 17:48:40 +0530

    third

---
+++ majority element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int res=0;
+        int count=1;
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[res]==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count==0){
+                res=i;
+                count=1;
+            }
+        }
+        count=0;
+        for(int i=0;i<n;i++){
+            if(nums[res]==nums[i]){
+                count++;
+            }
+        }
+        if(count<=n/2){
+            res=-1;
+        }
+        return nums[res];
+    }
+};
