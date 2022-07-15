e7d35d5dc376a57aef2f979a74591a041f7d57f4
Shubham <singhshubham1211@gmail.com>
2022-07-15 22:12:16 +0530

    change

--- missing number.cpp
+++ missing number.cpp
@@ -9,3 +9,32 @@ public:
         return sum2-sum;
     }
 };
+
+
+//solution through binary search
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//             sort(nums.begin(),nums.end());
+
+
+//     int start=0;
+//     int end=nums.size()-1;
+    
+//     while(start<= end){
+//         int mid= start +(end - start)/2;
+        
+//         if(nums[mid] == mid){  // since the array is sorted nums[mid] = mid if left part have all elements  so the missing number must be in right half
+
+//             start = mid+1;     
+//         }
+// 		// else the missing number is in left side
+//         else{
+//             end = mid-1;
+//         }
+//        // mid= start +(end - start)/2;
+//     }
+//     return start;
+        
+//     }
+// };
